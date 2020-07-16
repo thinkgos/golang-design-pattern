04162a39b160e4736f10e352d6e4c0de1ad3129f
mo <thinkgo@aliyun.com>
2020-07-16 18:11:53 +0800

    fix 原型模式

--- 01-创建型模式/05_prototype/prototype_test.go
+++ 01-创建型模式/05_prototype/prototype_test.go
@@ -2,8 +2,6 @@ package prototype
 
 import "testing"
 
-var manager *PrototypeManager
-
 type Type1 struct {
 	name string
 }
@@ -13,40 +11,16 @@ func (t *Type1) Clone() Cloneable {
 	return &tc
 }
 
-type Type2 struct {
-	name string
-}
-
-func (t *Type2) Clone() Cloneable {
-	tc := *t
-	return &tc
-}
-
-func TestClone(t *testing.T) {
-	t1 := manager.Get("t1")
-
-	t2 := t1.Clone()
-
-	if t1 == t2 {
-		t.Fatal("error! get clone not working")
-	}
+func (t *Type1) Name() string {
+	return t.name
 }
 
 func TestCloneFromManager(t *testing.T) {
-	c := manager.Get("t1").Clone()
+	manager := NewPrototypeManager()
+	manager.Set("t1", &Type1{name: "type1"})
 
-	t1 := c.(*Type1)
-	if t1.name != "type1" {
+	t1 := manager.Get("t1").Clone()
+	if t1.Name() != "type1" {
 		t.Fatal("error")
 	}
-
-}
-
-func init() {
-	manager = NewPrototypeManager()
-
-	t1 := &Type1{
-		name: "type1",
-	}
-	manager.Set("t1", t1)
 }
