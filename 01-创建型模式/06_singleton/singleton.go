4d2e36d88a300bb1e615bf504e03dfc6cf824e43
mo <thinkgo@aliyun.com>
2020-07-21 08:43:55 +0800

    fix 单例模式

--- 01-创建型模式/06_singleton/singleton.go
+++ 01-创建型模式/06_singleton/singleton.go
@@ -2,17 +2,16 @@ package singleton
 
 import "sync"
 
-//Singleton 是单例模式类
-type Singleton struct{}
-
 var singleton *Singleton
 var once sync.Once
 
-//GetInstance 用于获取单例模式对象
+// Singleton 单例模式类
+type Singleton struct{}
+
+// GetInstance 获取单例模式对象
 func GetInstance() *Singleton {
 	once.Do(func() {
-		singleton = &Singleton{}
+		singleton = new(Singleton)
 	})
-
 	return singleton
 }
--- 01-创建型模式/06_singleton/singleton_test.go
+++ 01-创建型模式/06_singleton/singleton_test.go
@@ -3,22 +3,23 @@ package singleton
 import (
 	"sync"
 	"testing"
-)
 
-const parCount = 100
+	"github.com/stretchr/testify/require"
+)
 
 func TestSingleton(t *testing.T) {
 	ins1 := GetInstance()
 	ins2 := GetInstance()
-	if ins1 != ins2 {
-		t.Fatal("instance is not equal")
-	}
+	require.Equal(t, ins1, ins2)
 }
 
 func TestParallelSingleton(t *testing.T) {
+	const parCount = 100
+
 	wg := sync.WaitGroup{}
+	instances := make([]*Singleton, parCount)
+
 	wg.Add(parCount)
-	instances := [parCount]*Singleton{}
 	for i := 0; i < parCount; i++ {
 		go func(index int) {
 			instances[index] = GetInstance()
@@ -26,9 +27,8 @@ func TestParallelSingleton(t *testing.T) {
 		}(i)
 	}
 	wg.Wait()
+
 	for i := 1; i < parCount; i++ {
-		if instances[i] != instances[i-1] {
-			t.Fatal("instance is not equal")
-		}
+		require.Equal(t, instances[i-1], instances[i])
 	}
 }
